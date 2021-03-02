b97b5a1c1e98794c0bd24aa1f40bda37d9b26cae
RPIUdishan <it18082234@my.sliit.lk>
2021-03-02 18:06:03 +0530

    styling packeges added

---
+++ countdown_timer/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const header = () => {
+    return(
+        <div className='container'>
+            <h1>Countdown Timer</h1>
+        </div>
+        
+    );
+}
+
+export default header;
